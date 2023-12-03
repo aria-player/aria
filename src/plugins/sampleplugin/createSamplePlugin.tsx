28103576b1ffe736255d4c9b632360ea161e80e8
mark-gl <26033115+mark-gl@users.noreply.github.com>
2023-12-03 18:52:49 +0000

    Add basic localisation to sample plugin

--- src/plugins/sampleplugin/createSamplePlugin.tsx
+++ src/plugins/sampleplugin/createSamplePlugin.tsx
@@ -2,24 +2,30 @@ import {
   PluginCallbacks,
   PluginHandle
 } from "../../features/plugins/pluginsTypes";
+import i18n from "i18next";
+import { useTranslation } from "react-i18next";
+import en_us from "./locales/en_us/translation.json";
 
 export function createSamplePlugin(
   _config: unknown,
   host: PluginCallbacks
 ): PluginHandle {
   console.log("Created sampleplugin");
+  i18n.addResourceBundle("en-US", "sampleplugin", en_us);
 
   return {
     Config() {
+      const { t } = useTranslation();
+
       return (
         <div>
-          Sample Plugin
+          {t("sampleplugin:config.header")}
           <button
             onClick={() => {
               host.pong("ping");
             }}
           >
-            Send message to app
+            {t("sampleplugin:config.button")}
           </button>
         </div>
       );
@@ -30,6 +36,7 @@ export function createSamplePlugin(
     },
 
     dispose() {
+      i18n.removeResourceBundle("en-US", "sampleplugin");
       console.log("Disposed sampleplugin");
     }
   };
