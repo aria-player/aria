a17693a058ac7a857f297d0de5db2bea006328d9
mark-gl <26033115+mark-gl@users.noreply.github.com>
2024-02-23 17:47:07 +0000

    Add album overlay when clicked

--- src/components/views/AlbumGrid.tsx
+++ src/components/views/AlbumGrid.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import { useAppSelector } from "../../app/hooks";
 import { selectVisibleTracks } from "../../features/sharedSelectors";
 import { selectAllTracks } from "../../features/tracks/tracksSlice";
 import { Track } from "../../features/tracks/tracksTypes";
 import { AlbumArt } from "../AlbumArt";
 import styles from "./AlbumGrid.module.css";
+import LeftArrow from "../../assets/arrow-left-solid.svg?react";
 
 export default function AlbumGrid() {
   const libraryTracks = useAppSelector(selectAllTracks);
@@ -19,6 +21,9 @@ export default function AlbumGrid() {
   );
   // TODO: Decide how to handle albums with the same name
   const visibleAlbums = [...new Set(visibleTracks.map((track) => track.album))];
+  const [selectedItem, setSelectedItem] = useState<string | null | undefined>(
+    null
+  );
 
   return (
     <div style={{ overflowY: "auto" }}>
@@ -34,7 +39,10 @@ export default function AlbumGrid() {
                     : "none"
                 }}
               >
-                <button className={styles.gridItem}>
+                <button
+                  className={styles.gridItem}
+                  onClick={() => setSelectedItem(track.album)}
+                >
                   <AlbumArt track={track as Track} />
                 </button>
                 <div className={`${styles.albumText} ${styles.albumTitle}`}>
@@ -46,6 +54,31 @@ export default function AlbumGrid() {
               </div>
             );
         })}
+        {selectedItem && (
+          <div
+            className={styles.detailOuter}
+            onClick={(e) => {
+              setSelectedItem(null);
+            }}
+          >
+            <div
+              className={styles.detailInner}
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+            >
+              <button
+                className={styles.backButton}
+                onClick={(e) => {
+                  setSelectedItem(null);
+                }}
+              >
+                <LeftArrow />
+              </button>
+              <div>{selectedItem}</div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
