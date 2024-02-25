66e076e677e50a7df1efa85d7c26ae587f188807
mark-gl <26033115+mark-gl@users.noreply.github.com>
2024-02-25 01:05:59 +0000

    Add factory reset button

--- src/components/pages/settings/AboutPage.tsx
+++ src/components/pages/settings/AboutPage.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 import styles from "./settings.module.css";
+import localforage from "localforage";
 
 export function AboutPage() {
   const { t } = useTranslation();
@@ -8,6 +9,18 @@ export function AboutPage() {
     <>
       <h4 className={styles.header}>{t("settings.about.version")}</h4>
       1.0
+      <br />
+      <button
+        onClick={async () => {
+          const confirmed = await confirm(t("settings.about.confirmReset"));
+          if (confirmed) {
+            await localforage.clear();
+            window.location.reload();
+          }
+        }}
+      >
+        {t("settings.about.reset")}
+      </button>
     </>
   );
 }
