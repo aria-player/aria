3a6a277ef5140eb55eeeca6e9c1cf08ea37e5e76
mark-gl <26033115+mark-gl@users.noreply.github.com>
2024-03-11 17:49:03 +0000

    Add initial test method using mocked platform/drag backend

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,11 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Platform, PlatformContext } from "./contexts/PlatformContext";
+import { TestBackend } from "react-dnd-test-backend";
+import { DndProvider } from "react-dnd";
+import { ModuleRegistry } from "@ag-grid-community/core";
+import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
 
 test("renders loading state by default", () => {
   class ResizeObserver {
@@ -22,4 +27,30 @@ test("renders loading state by default", () => {
 
   const loadingElement = document.querySelector("[class*='loading']");
   expect(loadingElement).toBeInTheDocument();
+});
+
+test("renders search bar when loaded", () => {
+  const mockPlatform = {
+    platform: Platform.Web,
+    fullscreen: null,
+    decorations: null,
+    minimiseToTray: null,
+    setDecorations: () => {},
+    setMinimiseToTray: () => {}
+  };
+  ModuleRegistry.registerModules([ClientSideRowModelModule]);
+
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <PlatformContext.Provider value={mockPlatform}>
+          <DndProvider backend={TestBackend}>
+            <App />
+          </DndProvider>
+        </PlatformContext.Provider>
+      </BrowserRouter>
+    </Provider>
+  );
+  const searchBar = document.querySelector("[class*='search']");
+  expect(searchBar).toBeInTheDocument();
 });
