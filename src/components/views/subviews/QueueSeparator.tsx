24e0d0caaee0a1607f1b732c684c56c3631cd300
mark-gl <26033115+mark-gl@users.noreply.github.com>
2024-03-18 20:45:38 +0000

    Show current playlist name/track group in queue separator

--- src/components/views/subviews/QueueSeparator.tsx
+++ src/components/views/subviews/QueueSeparator.tsx
@@ -2,13 +2,30 @@ import { ICellRendererParams } from "@ag-grid-community/core";
 import { useAppSelector } from "../../../app/hooks";
 import { selectQueueSource } from "../../../features/player/playerSlice";
 import styles from "./QueueSeparator.module.css";
+import { selectCurrentPlaylist } from "../../../features/currentSelectors";
+import { selectPlaylistsLayoutItemById } from "../../../features/playlists/playlistsSlice";
+import { useTranslation } from "react-i18next";
 
 export default function QueueSeparator(props: ICellRendererParams) {
+  const { t } = useTranslation();
   const queueSource = useAppSelector(selectQueueSource);
+  const currentPlaylistId = useAppSelector(selectCurrentPlaylist)?.id;
+  const playlistName = useAppSelector((state) =>
+    selectPlaylistsLayoutItemById(state, currentPlaylistId ?? "")
+  )?.name;
+  const currentGroup = useAppSelector(
+    (state) => state.player.queueSelectedGroup
+  );
 
-  return props.node.rowIndex == 0 ? (
-    <div className={styles.separator}>Current track</div>
+  return props.node.rowIndex === 0 ? (
+    <div className={styles.separator}>{t("queue.currentTrack")}</div>
   ) : (
-    <div className={styles.separator}>Playing from: {queueSource}</div>
+    <div className={styles.separator}>
+      {t("queue.playingFrom")}
+      <span className={styles.source}>
+        {playlistName || currentGroup || t(`views.${queueSource}`)}
+        {playlistName && currentGroup && ` / ${currentGroup}`}
+      </span>
+    </div>
   );
 }
