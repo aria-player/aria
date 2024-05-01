c868956d4069b8c4e6c150ed6f0816067e222f0c
mark-gl <26033115+mark-gl@users.noreply.github.com>
2024-05-01 18:59:21 +0100

    Add metadata scan progress to library settings page

--- src/components/pages/settings/LibraryPage.tsx
+++ src/components/pages/settings/LibraryPage.tsx
@@ -9,23 +9,41 @@ import {
   selectActivePlugins,
   selectPluginData
 } from "../../../features/plugins/pluginsSlice";
+import { selectAllTracks } from "../../../features/tracks/tracksSlice";
 
 export function LibraryPage() {
   const { t } = useTranslation();
   const activePlugins = useAppSelector(selectActivePlugins);
   const pluginData = useAppSelector(selectPluginData);
+  const allTracks = useAppSelector(selectAllTracks);
+  const scannedTracks = allTracks.filter(
+    (track) => track.metadataLoaded
+  ).length;
+  const totalTracks = allTracks.length;
   const activeSourcePlugins = activePlugins.filter(
     (plugin: PluginId) => plugins[plugin].type === "source"
   );
 
   return (
     <>
-      {activeSourcePlugins.length == 0 && (
+      {activeSourcePlugins.length == 0 ? (
         <>
           <h4>{t("settings.library.noSources")}</h4>
           <i className={styles.header}>
             {t("settings.library.noSourcesDetail")}
           </i>
+        </>
+      ) : (
+        <>
+          <h4>{"Library info"}</h4>
+          {scannedTracks == totalTracks ? (
+            `Total tracks in library: ${totalTracks}`
+          ) : (
+            <>
+              Scanning in progress ({scannedTracks}/{totalTracks}) <br />
+              <progress value={scannedTracks} max={totalTracks} />
+            </>
+          )}
         </>
       )}
       {activePlugins?.map((plugin: PluginId) => {
