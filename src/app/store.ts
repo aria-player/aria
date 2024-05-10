3922779d710c0ab7ba1b6a4d14840428fac5f855
mark-gl <26033115+mark-gl@users.noreply.github.com>
2024-05-10 19:22:35 +0100

    Persist current queue index

--- src/app/store.ts
+++ src/app/store.ts
@@ -50,7 +50,7 @@ const reducer = combineReducers({
     {
       key: "player",
       storage,
-      blacklist: ["status", "queueIndex"]
+      blacklist: ["status"]
     },
     playerReducer
   ),
