4a01dadcacd89d1c4f1f0062ff4d123ac06d3065
mark-gl <26033115+mark-gl@users.noreply.github.com>
2024-06-16 16:12:40 +0100

    Display title matches first in search results

--- src/app/search.ts
+++ src/app/search.ts
@@ -1,17 +1,27 @@
 import { Track } from "../features/tracks/tracksTypes";
 
 export const searchTracks = (tracks: Track[], search: string): Track[] => {
-  return tracks.filter((track) => {
-    return [
-      track.title,
-      track.artist,
-      track.albumArtist,
-      track.album,
-      track.genre,
-      track.composer,
-      track.comments
-    ]
-      .flat()
-      .some((value) => value?.toLowerCase().includes(search.toLowerCase()));
-  });
+  // TODO: More elaborate search result sorting (e.g. prioritising whole word matches)
+  const searchLowercase = search.toLowerCase();
+  return tracks
+    .filter((track) => {
+      return [
+        track.title,
+        track.artist,
+        track.albumArtist,
+        track.album,
+        track.genre,
+        track.composer,
+        track.comments
+      ]
+        .flat()
+        .some((value) => value?.toLowerCase().includes(searchLowercase));
+    })
+    .sort((a, b) =>
+      a.title.toLowerCase().includes(searchLowercase)
+        ? -1
+        : b.title.toLowerCase().includes(searchLowercase)
+          ? 1
+          : 0
+    );
 };
