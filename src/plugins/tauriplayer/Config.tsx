96754d91da0498a9028057adfd87fe4c65d884c3
mark-gl <26033115+mark-gl@users.noreply.github.com>
2024-07-27 00:27:21 +0100

    Adjust styling and layout of Tauri player config

--- src/plugins/tauriplayer/Config.tsx
+++ src/plugins/tauriplayer/Config.tsx
@@ -1,4 +1,6 @@
 import { useTranslation } from "react-i18next";
+import styles from "./Config.module.css";
+import RemoveIcon from "./assets/trash-can-solid.svg?react";
 
 export function Config(props: {
   folders: Record<string, string[]>;
@@ -10,24 +12,55 @@ export function Config(props: {
   return (
     <>
       <h4>{t("tauriplayer:config.folders")}</h4>
+      <table className={styles.folders}>
+        <thead>
+          <tr>
+            <th className={styles.pathColumn}>
+              {t("tauriplayer:config.path")}
+            </th>
+            <th className={styles.separatedColumn}>
+              {t("tauriplayer:config.tracks")}
+            </th>
+            <th className={styles.separatedColumn}>
+              {t("tauriplayer:config.actions")}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(props.folders).map((folder) => (
+            <tr key={folder}>
+              <td className={styles.pathColumn}>{folder}</td>
+              <td>{props.folders[folder]?.length}</td>
+              <td>
+                <button
+                  onClick={() => {
+                    props.removeFolder(folder);
+                  }}
+                  title={t("tauriplayer:config.removeFolder")}
+                  // TODO: This is a workaround since CSS modules do not seem to take precedence over 'settings.module.css'
+                  style={{
+                    padding: 0,
+                    border: "none"
+                  }}
+                  className={styles.removeButton}
+                >
+                  <RemoveIcon />
+                </button>
+              </td>
+            </tr>
+          ))}
+          {Object.keys(props.folders).length === 0 ? (
+            <td colSpan={3}>
+              <div className={styles.noFolders}>
+                <i>{t("tauriplayer:config.noFolders")}</i>
+              </div>
+            </td>
+          ) : null}
+        </tbody>
+      </table>
       <button onClick={props.addFolder}>
         {t("tauriplayer:config.addFolder")}
       </button>
-      <ul>
-        {Object.keys(props.folders).map((folder) => (
-          <li key={folder}>
-            {folder}
-            <button
-              onClick={() => {
-                props.removeFolder(folder);
-              }}
-            >
-              {t("tauriplayer:config.remove")}
-            </button>
-            {props.folders[folder]?.length}
-          </li>
-        ))}
-      </ul>
     </>
   );
 }
