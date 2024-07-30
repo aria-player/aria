ad9c157bbf94f7a5b277cc513f533bd8404f96f3
mark-gl <26033115+mark-gl@users.noreply.github.com>
2024-07-30 20:42:28 +0100

    Add toggle for error details on crash page

--- src/components/pages/CrashPage.tsx
+++ src/components/pages/CrashPage.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { PlatformContext, Platform } from "../../contexts/PlatformContext";
 import { MacTitleBar } from "../platforms/mac/MacTitleBar";
 import { WindowsMenuBar } from "../platforms/windows/WindowsMenuBar";
@@ -6,6 +6,7 @@ import styles from "./CrashPage.module.css";
 
 export const CrashPage = ({ error }: { error: DOMException }) => {
   const { platform, fullscreen } = useContext(PlatformContext);
+  const [showDetails, setShowDetails] = useState(false);
   const contextError = platform == Platform.Unknown;
   const tauriDragRegion = contextError
     ? { "data-tauri-drag-region": true }
@@ -21,6 +22,21 @@ export const CrashPage = ({ error }: { error: DOMException }) => {
           wrong
         </h1>
         <p {...tauriDragRegion}>Error message: &quot;{error.message}&quot;</p>
+        <div className={styles.details} {...tauriDragRegion}>
+          <button
+            onClick={() => {
+              setShowDetails(!showDetails);
+            }}
+          >
+            {showDetails ? "Hide error details" : "Show error details"}
+          </button>
+          <div
+            className={styles.collapsible}
+            style={{ display: showDetails ? "block" : "none" }}
+          >
+            <pre>{error.stack}</pre>
+          </div>
+        </div>
       </div>
     </div>
   );
