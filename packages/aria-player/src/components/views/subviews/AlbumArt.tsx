30b0034d7f3141e007e7d1f32099e12188875515
mark-gl <26033115+mark-gl@users.noreply.github.com>
2025-11-29 16:37:00 +0000

    Fix album art flashing after updates to track data

--- packages/aria-player/src/components/views/subviews/AlbumArt.tsx
+++ packages/aria-player/src/components/views/subviews/AlbumArt.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
 import MusicIcon from "../../../assets/music.svg?react";
 import { Track } from "../../../../../types/tracks";
 import styles from "./AlbumArt.module.css";
@@ -14,29 +14,36 @@ export const AlbumArt = ({
   album?: AlbumDetails;
 }) => {
   const { artworkCache } = useContext(ArtworkContext);
-  const [artwork, setArtwork] = useState<string | null>(null);
+  const [fetchedArtwork, setFetchedArtwork] = useState<string | null>(null);
+
+  const item = track ?? album;
+  const artworkUri = item?.artworkUri;
+  const source = item?.source;
+
+  const artwork = useMemo(() => {
+    if (artworkUri) {
+      return artworkCache[artworkUri] || fetchedArtwork;
+    }
+    return null;
+  }, [artworkUri, artworkCache, fetchedArtwork]);
 
   useEffect(() => {
-    const item = track ?? album;
-    if (item != undefined) {
-      if (item.artworkUri && artworkCache[item.artworkUri]) {
-        setArtwork(artworkCache[item.artworkUri]);
-        return;
-      }
-      if (
-        item.artworkUri &&
-        getSourceHandle(item.source)?.getTrackArtwork != undefined
-      ) {
-        getSourceHandle(item.source)
-          ?.getTrackArtwork?.(item.artworkUri)
-          .then((coverArtData) => {
-            setArtwork(coverArtData ?? null);
-            return;
-          });
-      }
+    if (!artworkUri || !source) {
+      setFetchedArtwork(null);
+      return;
     }
-    setArtwork(null);
-  }, [artworkCache, track, album]);
+    if (artworkCache[artworkUri]) {
+      return;
+    }
+    const handle = getSourceHandle(source);
+    if (handle?.getTrackArtwork) {
+      handle.getTrackArtwork(artworkUri).then((coverArtData) => {
+        if (coverArtData) {
+          setFetchedArtwork(coverArtData);
+        }
+      });
+    }
+  }, [artworkUri, source, artworkCache]);
 
   return artwork ? (
     <img
