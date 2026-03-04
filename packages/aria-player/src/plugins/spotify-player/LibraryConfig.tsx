4770e27dca88675fa76803a7b9e494742d1a4601
mark-gl <26033115+mark-gl@users.noreply.github.com>
2026-03-04 22:15:01 +0000

    Hide advanced Spotify settings in production

--- packages/aria-player/src/plugins/spotify-player/LibraryConfig.tsx
+++ packages/aria-player/src/plugins/spotify-player/LibraryConfig.tsx
@@ -57,17 +57,19 @@ export default function LibraryConfig(props: {
           {t("settings.logInWithSpotify")}
         </button>
       )}
-      <p>
-        <button
-          className={styles.advancedSettingsButton}
-          onClick={() => {
-            setShowAdvancedSettings(!showAdvancedSettings);
-          }}
-        >
-          {t("settings.toggleAdvancedSettings")}
-        </button>
-      </p>
-      {showAdvancedSettings && (
+      {import.meta.env.DEV && (
+        <p>
+          <button
+            className={styles.advancedSettingsButton}
+            onClick={() => {
+              setShowAdvancedSettings(!showAdvancedSettings);
+            }}
+          >
+            {t("settings.toggleAdvancedSettings")}
+          </button>
+        </p>
+      )}
+      {import.meta.env.DEV && showAdvancedSettings && (
         <p>
           {t("settings.redirectUri")}
           <br />
